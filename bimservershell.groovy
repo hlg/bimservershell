f0bc0fa1b502369986d757a21f1f4962ef38bd0b
hlg <public.void@gmx.de>
2017-08-16 11:54:47 +0800

    fix dependencies, project name argument, run shell

--- bimservershell.groovy
+++ bimservershell.groovy
@@ -1,27 +1,27 @@
-@Grapes(
-    @Grab(group='org.opensourcebim', module='bimserverclientlib', version='1.5.76')
-)
+@Grapes([
+    @Grab(group='org.opensourcebim', module='bimserverclientlib', version='1.5.76'),
+    @Grab(group='org.opensourcebim', module='pluginbase', version='1.5.76')
+])
 
 import org.bimserver.client.BimServerClient;
 import org.bimserver.client.json.JsonBimServerClientFactory;
 import org.bimserver.interfaces.objects.SProject;
-import org.bimserver.shared.ChannelConnectionException;
 import org.bimserver.shared.UsernamePasswordAuthenticationInfo;
-import org.bimserver.shared.exceptions.BimServerClientException;
-import org.bimserver.shared.exceptions.PublicInterfaceNotFoundException;
-import org.bimserver.shared.exceptions.ServiceException;
+import org.bimserver.emf.IfcModelInterface;
 
 import org.codehaus.groovy.tools.shell.Groovysh
 import org.codehaus.groovy.tools.shell.IO 
 
-JsonBimServerClientFactory factory = new JsonBimServerClientFactory("http://localhost:8080")
-BimServerClient client = factory.create(new UsernamePasswordAuthenticationInfo("admin@bimserver.org", "admin"))
+JsonBimServerClientFactory factory = new JsonBimServerClientFactory("http://localhost:8082")
+BimServerClient client = factory.create(new UsernamePasswordAuthenticationInfo("admin@localhost", "admin"))
+SProject project = client.serviceInterface.getProjectsByName(args[0])[0]
 
-SProject project = cient.serviceInterface.getProjectByPoid(args[1])
 IfcModelInterface model = client.getModel(project, project.lastRevisionId, true, false) // load deep, no geometry? interface might have changed, this one private now
 
+println "loading project $project.oid into shell context"
 shell = new Groovysh([model: model] as Binding, new IO())
-shell.run()
+shell.run('')
+
    
 // https://github.com/groovy/groovy-core/tree/master/subprojects/groovy-groovysh/src/main/groovy/org/codehaus/groovy/tools/shell/commands
 
